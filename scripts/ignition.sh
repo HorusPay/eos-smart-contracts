61fc7d92b06d51a356bcaaec57652ce326877b79
jackdisalvatore <jackp.disalvatore@gmail.com>
2018-09-16 16:06:11 -0400

    modifying so that ignition.sh loads both the horustokenio contract and the horus.bancor contract assuming that repos are places inside /HorusPay/ analogously to the Github structure.

--- scripts/ignition.sh
+++ scripts/ignition.sh
@@ -8,32 +8,63 @@ source setup/setup_env.sh
 pkill nodeos
 pkill keosd
 
-# echo "Launching nodeos................."
-gnome-terminal -e 'nodeos -e -p eosio --delete-all-blocks --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin'
+# check operating system
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+echo "Launching nodeos................."
+
+if [ ${machine} =  "Linux" ]; then
+   echo "You are running Linux"
+   gnome-terminal -e 'nodeos -e -p eosio --delete-all-blocks --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin'
+elif [ ${machine} = "Mac" ]; then
+   echo "You are running Mac"
+   osascript -e 'tell app "Terminal"
+       do script "bash /Volumes/JackDiSalvatoreSSDT5/JacksFiles/EOSIO/HorusPay/eos-smart-contracts/scripts/actions/start_nodeos.sh"
+     end tell'
+fi
+
 # sleep .75
 sleep 5.0
 
 # Open your wallet. (If this is the first time, you may have to create a new default.wallet using the keys provided in setup/setup_env.sh)
 echo "---------- Unlocking Default Wallet ... ----------"
-bash ${SCRIPTS_HOME}/actions/unlock_wallet.sh 
+bash ${SCRIPTS_HOME}/actions/unlock_wallet.sh
 
 # create some accounts (acct1 and acct2), set in setup/setup_env.sh
-echo "---------- Creating Accounts ... ----------"
-bash ${SCRIPTS_HOME}/actions/accounts.sh
+#echo "---------- Creating Accounts ... ----------"
+#bash ${SCRIPTS_HOME}/actions/accounts.sh
 
 
 ################################ Now, upload some contracts #################
 # upload the bios contract and the eosio.token contract to create a new token
 echo "---------- Loading the bios contract... ----------"
-cleos set contract eosio ${EOS_HOME}/build/contracts/eosio.bios -p eosio@active
-# bash ${EOS_SCRIPTS_HOME}/actions/set_contract.sh
+cleos set contract eosio $EOS_HOME/build/contracts/eosio.bios -p eosio@active
+
+############################### create accounts #################################
+# horustokenio
+cleos create account eosio ${AUTH_ACCT} ${OWNER_PUBLIC_KEY} ${ACTIVE_PUBLIC_KEY}
+# horusbancor
+cleos create account eosio ${BANCOR_ACCT} ${OWNER_PUBLIC_KEY} ${ACTIVE_PUBLIC_KEY}
+
+sleep 1.0
 
 echo "---------- Loading the horusbancor contract ------"
-cleos set contract eosio ${CONTRACTS_HOME}/bancor/horusbancor -p eosio 
+cleos set contract horusbancor ${CONTRACTS_HOME}/horus.bancor/build/horusbancor -p horusbancor
+
+echo "---------- Loading the horustokenio contract ------"
+cleos set contact horustokenio ${CONTRACTS_HOME}/horus.tokenio/build/horustokenio -p horustokenio
 
 # create a new token
 # echo "---------- Creating a new token ... -------------"
-# bash ${EOS_SCRIPTS_HOME}/actions/create_token.sh
+bash ${EOS_SCRIPTS_HOME}/actions/create_token.sh
 
 # load the multi-index table example
 # cd ${EOS_CONTRACTS_HOME}/youvote
--- scripts/setup/setup_env.sh
+++ scripts/setup/setup_env.sh
@@ -4,11 +4,19 @@
 # @author gunnar pope
 # modified code from the brilliant work of @scott-schechter, horuspay.io. Thanks scott!
 
+if [ ${USER} = "jackdisalvatore" ]; then
+   EOS_HOME="/Volumes/JackDiSalvatoreSSDT5/JacksFiles/EOSIO/eos-v1.2/eos"
+fi
+
 # USAGE: Set the path (HORUS_HOME) of all your scripts relative to the Horuspay directory
 HORUS_HOME="${HOME}/repos/Horuspay"
 
+if [ ${USER} = "jackdisalvatore" ]; then
+   HORUS_HOME="/Volumes/JackDiSalvatoreSSDT5/JacksFiles/EOSIO/HorusPay"
+fi
+
 SCRIPTS_HOME="${HORUS_HOME}/eos-smart-contracts/scripts"
-CONTRACTS_HOME="${HORUS_HOME}/eos-smart-contracts"
+CONTRACTS_HOME="${HORUS_HOME}"
 
 ## keys used for development ############################################
 
@@ -19,6 +27,10 @@ EOSIO_PUBLIC_KEY='EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
 # the default wallet keys are:
 WALLET_PASSWORD='PW5Jw89KejypFTVNLsBg8FnvASPAJhoaJYxAK7d4EU7tLQX6bt7V1'
 
+if [ USER = "jackdisalvatore" ]; then
+   WALLET_PASSWORD='PW5JwkoRa4TVJppYJK8gvrbMx89FYF72UbT5d7sD938ys7Uq78dzK'
+fi
+
 # the default owner and active keys are
 OWNER_PRIVATE_KEY='5KHrknL6tADdwF8JTFSjAjGfFUAKtLiDdSQL24MokRCvkh7Fn9k'
 OWNER_PUBLIC_KEY='EOS8K4K68g5zSCsyQLn37TSeRNi3cguWAxZAT2iJdYekeHpMxNszo'
@@ -27,4 +39,4 @@ ACTIVE_PUBLIC_KEY='EOS7p8h2NZZhtqnuZLRZrnaB5qqLwjwjEMsN95vQY5eLL2ZDr6FBV'
 
 EOSIO_TOKEN="eosio.token"
 AUTH_ACCT="horustokenio"
-
+BANCOR_ACCT="horustokenio"
